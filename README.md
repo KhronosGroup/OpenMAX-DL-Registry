5e2107fd51e5a7c35b8247d5f6c4a4c8952eadcd
Jon Leech <oddhack@sonic.net>
2016-12-13 03:39:59 -0800

    Populate git repository from public and internal SVN repositories

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# OepnMAX-DL-Registry
-OpenMAX DL API Registry
+# OpenMAX-DL-Registry
+
+The OpenMAX-DL-Registry repository contains the OpenMAX DL API
+Registry, including specifications and headers.
+
+It is also used as a backing store for the web view of the registry at
+https://www.khronos.org/registry/omxdl/ ; commits to the master branch of
+this repository will be reflected there.
+
+In the past, the OpenMAX DL registry was maintained in a public Subversion
+repository. The history in that repository has not been imported to github,
+but it is still available at
+https://cvs.khronos.org/svn/repos/registry/trunk/public/omxdl/ .
+
+Interesting files in this repository include:
+
+* index.php - toplevel index page for the web view. This relies on PHP
+  include files found elsewhere on www.khronos.org and so is not very useful
+  in isolation.
+* api/ - OpenMAX DL header files
+* specs/ - OpenMAX DL specification documents.
+
+
+## Adding Extension Specifications
+
+At present there are no extensions defined for OpenMAX DL, and no process
+for extending it.
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+$static_title = 'Khronos OpenMAX DL Registry';
+$static_breadcrumb = array(
+    '/registry/' => 'Registry',
+    'NOLINK' => 'OpenMAX DL Registry'
+);
+include_once("../../assets/static_pages/khr_page_top.php");
+?>
+
+<h1 class="pagetitle">Khronos OpenMAX&trade; DL API Registry</h1>
+<div id="mainformat">
+
+
+<h1 style="text-align:center"> Khronos OpenMAX&trade; DL Registry </h1>
+
+<p> The OpenMAX DL registry contains specifications of the core API;
+    header files corresponding to the specifications; and
+    other related documentation. </p>
+
+<h6> OpenMAX DL Core API Specification and Headers </h6>
+
+<p> The current version of OpenMAX DL is 1.0.2. </p>
+
+<ul>
+<li> <a href="specs/omx_dl_v1_0_2.pdf"> OpenMAX DL 1.0.2
+     Specification </a> (updated December 21, 2007). </li>
+<li> <a href="specs/omx_dl_v1_0_2.release_notes.txt"> OpenMAX DL 1.0.2
+     Changelog</a> from version 1.0.1. </li>
+<li> <a href="api/1.0/omx_dl_v1_0_2.h.zip"> OpenMAX DL 1.0.2 Header
+     Files </a> (single .zip file). </li>
+</ul>
+
+<h6> Older Versions </h6>
+
+<p> Specs and headers for older versions of OpenMAX DL, provided for
+    reference. </p>
+
+<ul>
+<li> <a href="specs/omx_dl_v1_0_1.pdf"> OpenMAX DL 1.0.1
+     Specification </a> </li>
+<li> <a href="api/1.0/omx_dl_v1_0_1.h.zip"> OpenMAX AL 1.0.1 Header
+     Files </a> (single .zip file). </li>
+</ul>
+
+<h6> Providing Feedback on the Registry </h6>
+
+<p> Khronos welcomes comments and bug reports. To provide feedback on
+    OpenMAX DL (such as reporting missing content, bad links, or issues with
+    the specification or headers), file an issue in the <a
+    href="https://github.com/KhronosGroup/OpenMAX-DL-Registry/issues">
+    OpenMAX-DL-Registry </a> Github project. </p>
+
+<?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
+</body>
+</html>
